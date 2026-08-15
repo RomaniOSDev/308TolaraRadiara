ae2f251aa886879cedf5cb91e15e294ead2efc5c
Roman <romazaniosdev@gmail.com>
2026-08-15 22:21:12 +0300

    add view

--- 308TolaraRadiara/App/SceneDelegate.swift
+++ 308TolaraRadiara/App/SceneDelegate.swift
@@ -3,11 +3,13 @@ import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    private var launchFlowResolver: LaunchFlowResolver?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UIHostingController(rootView: ContentView())
+        launchFlowResolver = LaunchFlowResolver(window: window)
+        window?.rootViewController = launchFlowResolver?.resolveEntryViewController()
         window?.makeKeyAndVisible()
     }
 
